832922863b20a5b7c10c759561ac1205bc16ba6a
HyeBinYang <skdisk7368@gmail.com>
2025-04-14 16:19:18 +0900

    test: pre commit test

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -4,8 +4,6 @@ import WebView from 'react-native-webview';
 export default function Page() {
   const WEB_URL = 'http://192.168.0.9:3000';
 
-  var bb = 20;
-
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <WebView
