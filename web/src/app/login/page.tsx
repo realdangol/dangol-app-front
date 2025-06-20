1ebe405c717c79cb3db11af4949ebf66b6887a95
HyeBinYang <skdisk7368@gmail.com>
2025-06-20 20:45:40 +0900

    fix: login 페이지 레이아웃 수정

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -9,13 +9,13 @@ import { OAuthSignInButton } from './_components';
 
 const LoginPage = () => {
   return (
-    <>
+    <div className="w-full h-full flex flex-col items-center justify-center">
       <Image src={LogoImage} alt={'찐단골 로고'} width={180} height={180} />
       <div className="flex flex-col gap-4 mt-7 w-full px-4">
         <OAuthSignInButton.Naver />
         <OAuthSignInButton.Kakao />
       </div>
-    </>
+    </div>
   );
 };
 
