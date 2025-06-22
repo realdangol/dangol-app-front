3671c9d8d5021c64d3274ee999e05ad1364eee47
HyeBinYang <skdisk7368@gmail.com>
2025-06-23 00:42:47 +0900

    feat: BottomNavigation active color 적용

--- web/src/app/(afterLogin)/_components/BottomNavigation.tsx
+++ web/src/app/(afterLogin)/_components/BottomNavigation.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { CheckList, MyPage, Notification, Shop } from '@realdangol/jjindangol-ui';
+import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
@@ -15,11 +16,13 @@ const BottomNavigation = () => {
       name: '단골집',
       icon: <Notification />,
       href: '/',
+      active: pathname === '/',
     },
     {
-      name: '?',
+      name: '단골가게',
       icon: <Shop />,
-      href: '/',
+      href: '/flyer',
+      active: pathname.startsWith('/flyer'),
     },
     {
       name: '주문내역',
@@ -34,13 +37,16 @@ const BottomNavigation = () => {
   ];
 
   return (
-    <nav className="fixed bottom-0 z-[100] w-screen h-[60px] bg-white">
+    <nav className="fixed bottom-0 z-[100] w-screen h-[60px] bg-white shadow-[0px_0px_2px_0px_#0000001F] shadow-[0px_0px_8px_0px_#0000001A]">
       <div className="max-w-[480px] h-full mx-auto flex">
         {items.map((item) => (
           <Link
             key={item.name}
             href={item.href}
-            className="h-full px-m flex-1 flex flex-col gap-xxs items-center justify-center text-[11px] text-neutral-600"
+            className={clsx(
+              'h-full px-m flex-1 flex flex-col gap-xxs items-center justify-center text-[11px]',
+              item.active ? 'text-brand-700' : 'text-neutral-600',
+            )}
           >
             {item.icon}
             {item.name}
